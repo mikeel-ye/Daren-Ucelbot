aedd23c19a6f5a58ec697b01501d887fc6cba8f9
Dareen <146856328+mikeel-ye@users.noreply.github.com>
2024-02-24 05:54:00 +0700

    Update _callback.py

--- AyiinXd/modules/_callback.py
+++ AyiinXd/modules/_callback.py
@@ -67,7 +67,7 @@ async def inline_handler(event):
     uid = user.id
     botusername = (await event.client.get_me()).username
     if event.query.user_id == uid and query.startswith(
-            "darenucelbot"):
+            "@AyiinChats"):
         buttons = paginate_help(0, dugmeler, "helpme")
         result = await event.builder.photo(
             file=logoyins,
