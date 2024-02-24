0f4e4abca5156c403024fa96f85f2c31e9c634d8
iamuput <yogaaasp73@gmail.com>
2024-02-24 07:20:26 +0700

    Update _callback.py

--- AyiinXd/modules/_callback.py
+++ AyiinXd/modules/_callback.py
@@ -16,10 +16,10 @@ dugmeler = CMD_HELP
 logo = var.ALIVE_LOGO
 logoyins = random.choice(
         [
-            "https://telegra.ph/file/9f8e73d387f25b7f27ce5.jpg",
-            "https://telegra.ph/file/c935d34b48e45fba22b03.jpg",
-            "https://telegra.ph/file/392f69c8717c91b1e8a3b.jpg",
-            "https://telegra.ph/file/4c5b756dd13d7a88c866b.jpg",
+            "https://telegra.ph//file/424ee3088eeaab2422533.jpg",
+            #"https://telegra.ph/file/c935d34b48e45fba22b03.jpg",
+            #"https://telegra.ph/file/392f69c8717c91b1e8a3b.jpg",
+            #"https://telegra.ph/file/4c5b756dd13d7a88c866b.jpg",
         ]
 )
 main_help_button = [
